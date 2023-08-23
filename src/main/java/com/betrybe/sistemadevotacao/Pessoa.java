dc75bfac47ef94c8f90b954c7f7f11f6df4a08dc
BrunoSouza <desouza88@hotmail.com>
2023-08-23 15:30:14 -0300

    feat: req02 class PessoaCandidata implemented

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -2,6 +2,7 @@ package com.betrybe.sistemadevotacao;
 
 public abstract class Pessoa {
   protected String nome;
+  public int altura;
 
   public Pessoa() {}
 
--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,39 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
 
+  int numero;
+  int votos;
+
+  /**
+   * PessoaCandidata.
+   */
+
+  public PessoaCandidata(String nome, int numero) {
+    super();
+
+    this.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  public int getNumero() {
+    return this.numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return this.votos;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  public void receberVoto() {
+    this.votos += 1;
+  }
 }
