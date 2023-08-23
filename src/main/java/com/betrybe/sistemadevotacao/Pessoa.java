2560243de41e85c9831fab64cabfa9d46c647c2e
BrunoSouza <desouza88@hotmail.com>
2023-08-22 22:45:37 -0300

    Req01

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,5 +1,17 @@
 package com.betrybe.sistemadevotacao;
 
-public class Pessoa {
-  
+public abstract class Pessoa {
+  protected String nome;
+
+  public Pessoa(String name) {
+    this.nome = name;
+  }
+
+  public String getNome() {
+    return this.nome;
+  }
+
+  public void setNome(String name) {
+    this.nome = name;
+  }
 }
