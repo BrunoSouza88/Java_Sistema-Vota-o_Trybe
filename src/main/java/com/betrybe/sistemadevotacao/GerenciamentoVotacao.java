721363b916457bdcf12c4bd93799dd2b44c1dcd4
BrunoSouza <desouza88@hotmail.com>
2023-08-23 16:30:52 -0300

    fix: req04

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -6,20 +6,24 @@ import java.util.List;
 /**
  * Classe para gerenciar o sistema de votação.
  */
-public class GerenciamentoVotacao {
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
   private List<PessoaCandidata> pessoasCandidatas;
   private List<PessoaEleitora> pessoasEleitoras;
+  private List<String> cpfsComputados;
 
+  /**
+   * Super.
+   */
   public GerenciamentoVotacao() {
     this.pessoasCandidatas = new ArrayList<>();
     this.pessoasEleitoras = new ArrayList<>();
+    this.cpfsComputados = new ArrayList<>();
   }
 
   /**
    * Cadastra uma pessoa candidata.
    */
-
   public void cadastrarPessoaCandidata(String nome, int numero) {
     for (PessoaCandidata candidata : pessoasCandidatas) {
       if (candidata.getNumero() == numero) {
@@ -45,5 +49,52 @@ public class GerenciamentoVotacao {
 
     PessoaEleitora novoEleitor = new PessoaEleitora(nome, cpf);
     pessoasEleitoras.add(novoEleitor);
+  }
+
+  /**
+   * Metodo para votar.
+   */
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+    if (cpfsComputados.contains(cpfPessoaEleitora)) {
+      System.out.println("Pessoa eleitora já votou!");
+      return;
+    }
+
+    PessoaEleitora eleitor = null;
+    for (PessoaEleitora e : pessoasEleitoras) {
+      if (e.getCpf().equals(cpfPessoaEleitora)) {
+        eleitor = e;
+        break;
+      }
+    }
+    if (eleitor == null) {
+      System.out.println("Pessoa eleitora não cadastrada!");
+      return;
+    }
+
+    PessoaCandidata candidata = null;
+    for (PessoaCandidata c : pessoasCandidatas) {
+      if (c.getNumero() == numeroPessoaCandidata) {
+        candidata = c;
+        break;
+      }
+    }
+    if (candidata == null) {
+      System.out.println("Pessoa candidata não cadastrada!");
+      return;
+    }
+
+    cpfsComputados.add(cpfPessoaEleitora);
+    candidata.receberVoto();
+    ;
+  }
+
+  /**
+   * Metodo para mostrar resultado.
+   */
+  public void mostrarResultado() {
+    for (PessoaCandidata candidata : pessoasCandidatas) {
+      System.out.println(candidata.getNome() + ": " + candidata.getVotos() + " votos");
+    }
   }
 }
